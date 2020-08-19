4122e8ab36125c825882d7f8e8146935f23be765
rev_matcher <gkaustav57@gmail.com>
2020-08-19 21:24:17 +0530

    created base div generator with all the features

---
+++ main.js
@@ -0,0 +1,38 @@
+const input =document.querySelector("mainInput");
+const addBtn =document.querySelector("addBtn");
+const list =document.querySelector("list-group")
+const form = document.querySelector("form");
+
+const randomID= () => Math.floor(Math.random()*1000);
+let items =
+[{
+    id: randomID(),
+    text: 'Item1'
+}];
+
+let formStatus = 'add';
+
+const generateItem = (item) =>
+{
+    const div = document.createElement("div");
+    div.setAttribute("class" ,
+    "list-group-item d-flex justify-content-between align items-center");
+    div.id =item.id;
+    const span =document.createElement("span");
+    span.setAttribute("class","text");
+    span.innerText =item.text;
+    const div2 = document.createElement("div");
+
+    const editButton = document.createElement("button");
+    editButton.setAttribute("class", "btn btn-dark editBtn btn-sm mr-1");
+    editButton.innerText ="Edit";
+
+    const delButton =document.createElement("button");
+    delButton.setAttribute("class", "btn btn-danger editBtn btn-sm mr-1");
+    delButton.innerText ="Delete";
+    div2.appendChild(editButton);
+    div2.appendChild(delButton);
+    div.appendChild(span);
+    div.appendChild(div2);
+    return div;
+}
